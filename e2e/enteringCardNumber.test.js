afd843c5d9e53fff6ce98e1e6401449ea455f152
Anatoly Batov <play.hoffa@gmail.com>
2024-02-12 14:40:22 +0300

    one

---
+++ e2e/enteringCardNumber.test.js
@@ -0,0 +1,57 @@
+import puppeteer from "puppeteer";
+import { fork } from "child_process";
+
+jest.setTimeout(60000);
+
+describe("enteringCardNumber", () => {
+  let browser = null;
+  let page = null;
+  let server = null;
+  // eslint-disable-next-line no-unused-vars
+  const baseUrl = "http://localhost:9000";
+
+  beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on("error", reject);
+      server.on("message", (message) => {
+        if (message === "ok") {
+          resolve();
+        }
+      });
+    });
+
+    browser = await puppeteer.launch({
+      headless: "new",
+      slowMo: 100,
+      // devtools: true,
+    });
+
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    server.kill();
+  });
+
+  test.each([
+    ["5586200023405365", "Verification completed successfully"],
+    ["5586200023405366", "Verification failed"],
+  ])(
+    "testing isValidNumber function with %i number card",
+    async (numberCard, message) => {
+      await page.goto("http://localhost:9000");
+      await page.waitForSelector(".form-control");
+      const input = await page.$(".form-control");
+      const btn = await page.$(".btn");
+      await input.type(numberCard);
+      await btn.click();
+      await page.waitForSelector(".result");
+      const result = await page.evaluate(() => {
+        return document.querySelector(".result").textContent;
+      });
+      await expect(result).toBe(message);
+    },
+  );
+});
